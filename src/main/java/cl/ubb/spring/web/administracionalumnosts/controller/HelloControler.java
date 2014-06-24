c73b7b645ff4862da10263e2b43f9f5ada0ab03f
xamzo <cristobal_ortg@hotmail.com>
2014-06-23 22:11:42 -0400

    First commit2

---
+++ src/main/java/cl/ubb/spring/web/administracionalumnosts/controller/HelloControler.java
@@ -0,0 +1,68 @@
+package cl.ubb.spring.web.administracionalumnosts.controller;
+
+import cl.ubb.spring.web.administracionalumnosts.model.Usuario;
+import cl.ubb.spring.web.administracionalumnosts.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cristobalortiz
+ * Date: 11-06-14
+ * Time: 23:39
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class HelloControler {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    public ModelAndView home() {
+
+        //Usuario usuario = new Usuario();
+
+        //usuario.setApellido("apellido");
+        //usuario.setNombre("nombre");
+        //usuario.setRut("rut");
+        //usuario.setUsername("ubb");
+        //usuario.setPassword("1234");
+        //usuarioService.create(usuario);
+
+        List<Usuario> user=usuarioService.getAll();
+        System.out.println("lista de usuario11: "+user);
+        String mensaje="Bienvenido (Mensaje desde el controller MVC)";
+
+        ModelAndView model = new ModelAndView("index");
+        model.addObject("mensaje", mensaje);
+
+        return model;
+
+    }
+
+    @RequestMapping(value = {"/admin**",}, method = RequestMethod.GET)
+    public ModelAndView admin() {
+
+
+        ModelAndView model = new ModelAndView("admin");
+        return model;
+
+    }
+
+
+    public UsuarioService getUsuarioService() {
+        return usuarioService;
+    }
+
+    public void setUsuarioService(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+}
